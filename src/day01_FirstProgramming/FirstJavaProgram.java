0b7f53efac5af069901ee503c566ee9c151fae0c
AyselDalcicek <48395471+AyselDalcicek@users.noreply.github.com>
2019-09-08 17:46:04 -0700

    Update FirstJavaProgram.java

--- src/day01_FirstProgramming/FirstJavaProgram.java
+++ src/day01_FirstProgramming/FirstJavaProgram.java
@@ -5,6 +5,7 @@ public class FirstJavaProgram {
 	public static void main (String[] args) { 
 		
 		System.out.println("Hello Cybertek Batch 12");
+		// New Update
 		System.out.println("Hello GitHub");
 		
 		
