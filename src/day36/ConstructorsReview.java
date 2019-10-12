5cba4161475e3c41f212164b64ef308eafa92c32
AyselDalcicek <ayseldalcicek07@gmail.com>
2019-10-12 15:58:54 -0700

    # WARNING: head commit changed in the meantime
    day 34 changes

---
+++ src/day36/ConstructorsReview.java
@@ -0,0 +1,34 @@
+package day36;
+
+
+public class ConstructorsReview {
+
+	public ConstructorsReview() {
+	
+	System.out.println("default constructor");
+
+}
+
+public ConstructorsReview(int a) {
+	
+	System.out.println("Constructor with argument int: "+a);
+	
+}
+
+
+public static void main(String[] args) {
+	
+	
+	ConstructorsReview obj = new ConstructorsReview(); 
+	
+//	ConstructorsReveiw obj2 = new ConstructorsReveiw("Batch 12"); 
+						// the constructor with arg of String is not exist
+	
+	ConstructorsReview obj2 = new ConstructorsReview(10); 
+	
+	
+        
+    }
+    
+
+}
---
+++ src/day36/WarmUp_RemoveDup_ArrayList.java
@@ -0,0 +1,82 @@
+package day36;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WarmUp_RemoveDup_ArrayList {
+	/*
+	    1. write a return method that can remove the duplicated objects from an Integer arraylist
+	    2. write a return method that can remove the duplicated objects from a String arraylist
+	    3. write a return method that can remove the duplicated objects from a Character arraylist
+	    4. write a return method that can remove the duplicated objects from a Double arraylist
+	 */
+	
+	
+	public static void main(String[] args) {
+		//Task01:
+		ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1,1,1,2,2,2,3,3) );
+		
+		list1 = RemoveDup(list1);
+		
+		System.out.println(list1); //  //[1,2,3]
+		
+	            
+				ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(4,4,5,5,6,6));
+	            System.out.println(list2); // [4, 4, 5, 5, 6, 6]
+	        
+	            list2 = RemoveDup(list2);
+	            
+	            System.out.println(list2); // [4, 5, 6]
+		
+		
+		//Task02:
+		String[] names = {"Aysel", "Aysel", "Bera", "Zeynep", "Kadir", "Zeynep", "Zeynep",};
+		ArrayList<String> list3 = new ArrayList<>( Arrays.asList( names ) );
+		
+		        // OR
+		//ArrayList<String> list3 = new ArrayList<>( Arrays.asList( 
+		                         //  "Aysel", "Aysel", "Bera", "Zeynep", "Kadir", "Zeynep", "Zeynep" ) );
+		
+		System.out.println(list3); // [Aysel, Aysel, Bera, Zeynep, Kadir, Zeynep, Zeynep]
+		
+		list3=RemoveDup2(list3);
+		System.out.println(list3);  // [Aysel, Bera, Zeynep, Kadir]
+		
+		
+		
+	}
+	//Task01:
+	public static ArrayList<Integer> RemoveDup(ArrayList<Integer> list) {
+		ArrayList<Integer> result = new ArrayList<>();
+	
+		/*
+		for (Integer each : list) {
+			if(! result.contains(each) ) {
+				result.add(each);
+		}
+	}
+	*/  // second way
+		for(int i =0; i<list.size(); i++) {
+			if( ! result.contains(list.get(i) ) ) {
+				result.add(list.get(i) );
+			}
+		}
+	return result;
+	}
+
+  // task02:
+	public static ArrayList<String>  RemoveDup2 ( ArrayList<String>  list ){
+        ArrayList<String> result = new ArrayList<>();  //[1,2,3]
+        
+        for(int i=0; i < list.size(); i++ ) {
+            if( ! result.contains( list.get(i) )  ) {
+                result.add( list.get(i)  );
+            }
+        }
+    
+        return result;
+    }
+	
+	// solve other tasks
+	
+}
